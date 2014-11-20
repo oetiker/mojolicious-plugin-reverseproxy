08f91c36f03101cbdcc483f8f915911bb11ca844
Tobias Oetiker <tobi@oetiker.ch>
2014-11-20 11:19:11 +0100

    switch to cloning and adapting the original reques

--- lib/Mojolicious/Plugin/ReverseProxy.pm
+++ lib/Mojolicious/Plugin/ReverseProxy.pm
@@ -9,35 +9,30 @@ use Mojo::UserAgent;
 
 my $ua = Mojo::UserAgent->new( cookie_jar => 0 );
 
-our $VERSION = '0.2';
-
+our $VERSION = '0.4';
 
 my $make_req = sub {
     my $ctrl = shift;
     my $dest_url = shift;
     my $loc_url = shift;
 
-    my $tx = Mojo::Transaction::HTTP->new;
-    my $nr = $tx->req;
+    my $tx = Mojo::Transaction::HTTP->new( req=> $ctrl->req->clone );
+    my $headers = $tx->req->headers;
+    $headers->remove('Host');
+    for (qw(Referer Origin)){
+        my $value = $headers->header($_) || next;
+        $value =~ s/^\Q${loc_url}/$dest_url/;
+        $headers->header($_ => $value );
+    }
 
-    # prepare requiest
-    $nr->url->parse($dest_url);
+    my $url = $tx->req->url;
+    $url->parse($dest_url);
+    $url->query($ctrl->req->url->query);
     my $req_path = $ctrl->req->url->path;
     my $base_path = Mojo::URL->new($loc_url)->path;
     $req_path =~ s/^\Q${base_path}//;
-    $nr->url->path($req_path);
-    $nr->url->query($ctrl->req->url->query);
-    $nr->method($ctrl->req->method);
-    $nr->body($ctrl->req->body);
+    $url->path($req_path);
 
-    # copy headers
-    my $headers = $ctrl->req->headers->to_hash(1);
-    delete $headers->{Host};
-    for (qw(Referer Origin)){
-        $headers->{$_}[0] =~ s/^\Q${loc_url}/$dest_url/ 
-            if ref $headers->{$_} eq 'ARRAY';
-    }
-    $nr->headers->from_hash($headers);
     return $tx;
 };
 
@@ -73,10 +68,9 @@ sub register {
                     $ctrl->render(status => 500, text => 'ERROR '. $err->{code} . ': ' . $err->{message});
                     return;
                 }
-                $log->debug($res->code);
                 if ($loc_url and $res->code =~ /^302$/){
                     my $location = $res->headers->location;
-                    if ($location =~ s/^\Q${dest_url}/$loc_url/){
+                    if ($location and $location =~ s/^\Q${dest_url}/$loc_url/){
                         $res->headers->location($location);
                     }
                 }
